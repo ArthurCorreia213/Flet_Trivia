559993479487a2108072e50a0b3cb3ac2cb8c61f
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-05-16 16:47:26 -0300

    Funcionando 👍

--- src/main.py
+++ src/main.py
@@ -1,65 +1,113 @@
 import flet as ft
 import requests
 from html import unescape
+from random import randint
 
-questao_num = 0
+questao_num=0
+numero_a_exibir = 0
+dados = []
+loop = 0
+pontuacao = 0
 
 def main(page: ft.Page):
+    global pontuacao
+
     questao_atual = ft.Text()
     questao_tipo = ft.Text()
     questao_dificuldade = ft.Text()
     questao_categoria = ft.Text()
     questao_body = ft.Text()
-    questao_correto = ft.Text()
-    questao_errados = ft.Text()
+    pontuacao_txt = ft.Text(str(pontuacao))
 
     resultado = ft.Text(key="texto_resultado")
 
-    def buscar_dados(e):
-        response = requests.get("https://opentdb.com/api.php?amount=1")
+    def buscar_dados(e,new_game=True):
+        global questao_num
+        global dados
+        global numero_a_exibir
+        global loop
+
+        if new_game == True or questao_num == 10:
+            if questao_num ==10:
+                loop+=1
+            response = requests.get("https://opentdb.com/api.php?amount=10")
+            print(len(response.json()['results']))
+            questao_num = 0
+            dados = response
+        else:
+            response = dados
+
         if response.status_code == 200:
-            data = response.json()['results'][0]
-            questao_tipo.value = unescape(data['type'])
-            questao_dificuldade.value = unescape(data['difficulty'])
-            questao_categoria.value = unescape(data['category'])
-            questao_body.value = unescape(data['question'])
+            if response.json()['response_code'] == 0:
+                data = response.json()['results'][questao_num-1]
+                questao_tipo.value = unescape(data['type'])
+                questao_dificuldade.value = unescape(data['difficulty'])
+                questao_categoria.value = unescape(data['category'])
+                questao_body.value = unescape(data['question'])
+                
+                i = 1
+                num_rnd = randint(1,4)
 
-            botao1 = ft.ElevatedButton(unescape(data['correct_answer']), on_click=lambda e: responder(unescape(data['correct_answer']),unescape(data['correct_answer'])))
-            page.add(botao1)
+                # if numero_a_exibir>0:
+                #     #print(page.controls[-4:-1])
+                #     while len(page.controls) >7:
+                #         if not page.controls[-1].key == 'texto_resultado':
+                #             page.controls.remove(page.controls[-1])
 
-            for resposta in data['incorrect_answers']:
-                botao_novo = ft.ElevatedButton(unescape(resposta), on_click=lambda e: responder(unescape(resposta),unescape(data['correct_answer'])))
-                page.add(botao_novo)
-            #questao_correto.value = unescape(data['correct_answer'])
-            #questao_errados.value = unescape(data['incorrect_answers'])
+                # Remover apenas botões de resposta (ElevatedButton com chave específica)
+                # for ctr in page.controls:
+                #     print(ctr)
 
-            global questao_num
-            questao_num +=1
-            questao_atual.value = f"Questão {questao_num}"
+                page.controls = [ctrl for ctrl in page.controls if not (isinstance(ctrl, ft.ElevatedButton) and ctrl.key and ctrl.key.startswith("botao_erro"))]
+                page.controls = [ctrl for ctrl in page.controls if not (isinstance(ctrl, ft.ElevatedButton) and ctrl.key and ctrl.key.startswith("RESPOSTA_CORRETA"))]
+                
+                # for ctr in page.controls:
+                #     print(ctr)
+
+                for resposta in data['incorrect_answers']:
+                    if i == num_rnd:
+                        botao1 = ft.ElevatedButton(unescape(data['correct_answer']), key="RESPOSTA_CORRETA", on_click=lambda e: responder(unescape(data['correct_answer']),unescape(data['correct_answer'])))
+                        page.add(botao1)
+
+                    botao_novo = ft.ElevatedButton(unescape(resposta), on_click=lambda e: responder(unescape(resposta),unescape(data['correct_answer'])), key=f'botao_erro1')
+                    i+=1
+                    page.controls.append(botao_novo)
+
+                if not encontrar_ctr("RESPOSTA_CORRETA"):
+                        print("ADICIONADO")
+                        botao1 = ft.ElevatedButton(unescape(data['correct_answer']), key="RESPOSTA_CORRETA", on_click=lambda e: responder(unescape(data['correct_answer']),unescape(data['correct_answer'])))
+                        page.controls.append(botao1)
+                    #print(page.get_control(botao1.uid))
+                #print(ft.ElevatedButton(unescape(data['correct_answer']), on_click=lambda e: responder(unescape(data['correct_answer']),unescape(data['correct_answer']))) in page.controls)
+
+            questao_num+=1
+            numero_a_exibir = questao_num + loop*10
+            questao_atual.value = f"Questão {numero_a_exibir}"
         else:
             resultado.value = "Erro"
         page.update()
 
     def responder(selecionada, correta):
         resultado.value = "Correto" if selecionada == correta else "Errado"
-        page.update()
-        #print(page.controls[-1])
-        #print(ft.Text(value='Correto') in page.controls)
-        #print(page.get_control("texto_resultado"))
+        resultado.size = 20
+
+        if resultado.value == "Correto":
+            global pontuacao
+            pontuacao +=1
+            pontuacao_txt.value = f'Pontuação: {pontuacao}'
         
-        #if page.get_control("texto_resultado"):
-        #    page.controls.remove(page.get_control("texto_resultado"))
             
+        page.update()
+        buscar_dados(0,False)
 
-        #if selecionada == correta:
-        #    result = ft.Text(value="Correto", key='texto_resultado')
-        #else:
-         #   result = ft.Text(value="Errado", key='texto_resultado')
-        #page.add(result)
+    def encontrar_ctr(key):
+        for ctr in page.controls:
+            if(isinstance(ctr, ft.ElevatedButton)):
+                if(ctr.key) == key:
+                    return ctr
+        return False
+    botao = ft.ElevatedButton("Gerar questão", on_click=buscar_dados)
 
-
-    botao = ft.ElevatedButton("Buscar dados da API", on_click=buscar_dados)
-
-    page.add(botao, questao_atual, questao_tipo, questao_dificuldade, questao_categoria, questao_body, questao_correto, questao_errados, resultado)
+    page.add(botao, pontuacao_txt, questao_atual, questao_tipo, questao_dificuldade, questao_categoria, questao_body, resultado)
 
 ft.app(main)
