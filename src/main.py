2791bfb720924f28fd489fbceafb196cdf1470c9
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2026-08-15 14:06:45 -0300

    huh

--- src/main.py
+++ src/main.py
@@ -344,8 +344,6 @@ def main(page: ft.Page):
         global dados
         global pontuacao
         global nome
-        global wrong
-        global right
 
         for ctr in page.controls:
             if ctr.key =='questao_atual':
